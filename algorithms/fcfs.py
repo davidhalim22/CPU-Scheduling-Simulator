9406a9f049c114045df264615055796cf6e4e21e
davidhalim22 <137604584+davidhalim22@users.noreply.github.com>
2026-06-15 03:54:40 +0700

    make little progress on the fcfs

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -1,2 +1,8 @@
 def fcfs(processes):
-    ...
+    processes = sorted(processes, key=lambda p: p.arrival)
+
+    current_time = 0
+    gantt = []
+    
+    for p in processes:       # Simulate the process execution
+        ...
