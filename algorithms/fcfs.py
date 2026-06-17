fe5100a5314ba78d987c3450328850cf637c7d84
davidhalim22 <137604584+davidhalim22@users.noreply.github.com>
2026-06-18 01:15:36 +0700

    added fcfs

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -5,4 +5,17 @@ def fcfs(processes):
     gantt = []
     
     for p in processes:       # Simulate the process execution
-        ...
+        if current_time < p.arrival:
+            current_time = p.arrival  # Wait for the process to arrive
+        
+        start_time = current_time
+        current_time += p.burst
+        end_time = current_time
+        
+        p.completion_time = end_time
+        p.turnaround_time = p.completion_time - p.arrival
+        p.waiting_time = p.turnaround_time - p.burst
+        
+        gantt.append((p.pid, start_time, end_time))
+        
+    return gantt, processes
